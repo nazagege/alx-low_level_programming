e0467e1638365b1f4c8b1d1831041d987a180172
Naza Banye <faithfulchinaza17@gmail.com>
2022-09-13 17:08:36 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,47 +1,31 @@
 #include "main.h"
+
 /**
- * times_table -prints tables
+ * jack_bauer -prints every minute of the day
  *
  * Return: Always 0.
  */
-void times_table(void)
+
+void jack_bauer(void)
+
 {
 int a;
 int b;
-int c;
-for (a = 0; a <= 9; a++)
+
+for (a = 0; a <= 23; a++)
 {
-for (b = 0; b <= 9; b++)
+for (b = 0; b <= 59; b++)
 {
-c = a * b;
-if ((c / 10) == 0)
-{
-if (b == 0)
-{
-_putchar ('0');
-}
-if (b != 0)
-{
-_putchar (' ');
-_putchar ((c % 10) + '0');
-}
-if (b < 9)
-{
-_putchar(',');
-_putchar (' ');
-}
-}
-else
-{
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
-if (b < 9)
-{
-_putchar(',');
-_putchar (' ');
-}
-}
-}
+_putchar (a / 10 + '0');
+_putchar (a % 10 + '0');
+_putchar (':');
+_putchar (b / 10 + '0');
+_putchar (b % 10 + '0');
 _putchar ('\n');
+
 }
+
+
+}
+
 }
